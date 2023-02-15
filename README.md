63792f50193bc53692f609be9c91bbd6859249b6
Mattijs Spierings <mattijs@earnr.com.au>
2023-02-15 15:45:54 +1100

    testing logging

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # 7 Digital Api example with Oauth
 
-Creating a valid signature can be a bit tricky but these examples my help you do you POSTs and GETs to 7Digitals API.
+Creating a valid signature can be a bit tricky but these examples may help you do you POSTs and GETs to 7Digitals API.
--- index.ts
+++ index.ts
@@ -6,8 +6,8 @@ dayjs.extend(utc);
 import qs from 'qs';
 
 const endpoint = 'https://api.7digital.com/1.2';
-const consumerKey = '[KEY]';
-const consumerSecret = '[SECRET]';
+const consumerKey = '[key]';
+const consumerSecret = '[secret]';
 
 
 const getPlayList = async (params: { id: string, country: string }) => {
@@ -43,7 +43,7 @@ const createUser = async (params: any) => {
   }
 }
 
-const createSubscription = async (params: { userId: string}) => {
+const createSubscription = async (params: { userId: string }) => {
   const url = `${endpoint}/user/unlimitedStreaming`;
 
   const activatedAt = dayjs().utc().format();
@@ -76,6 +76,27 @@ const createSubscription = async (params: { userId: string}) => {
   }
 }
 
+const logSubscriberStream = async (params: { userId: string, country: string, playLogItem: any[] }) => {
+  const { userId, country, playLogItem } = params;
+  const url = `${endpoint}/user/subscription/log?userId=${userId}&country=${country}`;
+
+  const data = { playLogItem };
+  const oath = createAuthorisation('POST', url, data);
+  console.log('========== OAUTH', oath)
+  try {
+    const response = await axios.post(url, data, {
+      headers: {
+        Authorization: oath,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      }
+    });
+    console.log('>>>', response.data)
+  } catch (error: any) {
+    console.error(error.response.data);
+  }
+}
+
 const createAuthorisation = (method: 'GET' | 'POST', url: string, params: any) => {
 
   const oauthParams = {
@@ -91,17 +112,33 @@ const createAuthorisation = (method: 'GET' | 'POST', url: string, params: any) =
     ...params,
   };
 
+  console.log('DATA TO BE AUTHORISED', data)
+  console.log('URL TO BE AUTHORISED', url)
+
   const signature = oauth.generate(method, url, data, consumerSecret, '',
     { encodeSignature: true });
 
+  console.log('>>>>>>>>>>>>>>>', oauthParams)
+
+  // manually create the oath string
+  let authParams: string[] = [`oauth_signature="${encodeURIComponent(signature)}"`];
+  for (const [key, value] of Object.entries(oauthParams)) {
+    authParams.push(`${key}="${value}"`);
+  }
+  const authStr = authParams.join(',');
+  console.log('=========1>', authStr)
+
+  // use QS to stringify the oauth params
   const authorisation = qs.stringify({
     ...oauthParams,
     oauth_signature: signature,
   }, {
     delimiter: ','
   });
+  console.log('=========2>', authorisation)
 
-  return `OAuth ${authorisation}`;
+  return `OAuth ${authStr}`;
+  // return `OAuth ${authorisation}`;
 }
 
 /**
@@ -109,9 +146,24 @@ const createAuthorisation = (method: 'GET' | 'POST', url: string, params: any) =
  */
 
 // getPlayList({
-//   id: '[id]',
+//   id: '60e2e3f565c9a45be0e0ca7e',
 //   country: 'AU'
 // });
+
+logSubscriberStream({
+  userId: 'brandewijn_20230214',
+  country: 'AU',
+  playLogItem:  [
+    {
+      "trackId": "149477",
+      "releaseId": "14411",
+      "formatId": "56",
+      "playMode": "online",
+      "dateTimePlayed": "2023-02-15T00:42:51.711Z",
+      "totalTimePlayed": 0,
+      "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+    }]
+});
 
 // createUser({
 //   userID: '[userId],
@@ -119,5 +171,5 @@ const createAuthorisation = (method: 'GET' | 'POST', url: string, params: any) =
 // });
 
 // createSubscription({
-//   userId: '[userId]',
+//   userId: 'brandewijn_20230214',
 // });
